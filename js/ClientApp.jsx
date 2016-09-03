b4064aa86232d8f207d9bfd9e8b1ded6d7e8c31e
Rishabh <rishabh.gupta998@gmail.com>
2016-09-03 15:36:15 +0530

    first commit

---
+++ js/ClientApp.jsx
@@ -0,0 +1,12 @@
+const React = require('react')
+const ReactDOM = require('react-dom')
+const Landing = require('./Landing')
+const {Router, Route, hashHistory} = require('react-router')
+
+const App = () => (
+  <Router history={hashHistory}>
+    <Route path='/' component={Landing} />
+  </Router>
+)
+
+ReactDOM.render(<App />, document.getElementById('app'))
---
+++ js/MyTitle.jsx
@@ -0,0 +1,16 @@
+var React = require('react')
+
+var MyTitle = React.createClass({
+  render () {
+    const style = {color: this.props.color}
+    return (
+      <div>
+        <h1 style={ style }>
+          {this.props.title}
+        </h1>
+      </div>
+    )
+  }
+})
+
+module.exports = MyTitle
